b3c1dd38095d4319345de4af45729c81f54e0d1f
aryelld <aryell@gmail.com>
2020-04-17 03:08:00 -0300

    excluindo dependecia

--- lib/pages/pointDetailPage.dart
+++ lib/pages/pointDetailPage.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
