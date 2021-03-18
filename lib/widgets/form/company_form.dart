214e0cd83c18a96eeb8d4d8c4ce4c6268fbbc7fd
Aryell Dias <aryelld@gmail.com>
2021-03-18 00:15:40 -0300

    terminando company form

--- lib/widgets/form/company_form.dart
+++ lib/widgets/form/company_form.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:perna/helpers/app_localizations.dart';
+import 'package:perna/models/bank_account.dart';
 import 'package:perna/models/company.dart';
 import 'package:perna/pages/bank_page.dart';
 import 'package:perna/widgets/button/add_button.dart';
 import 'package:perna/widgets/form/form_container.dart';
+import 'package:perna/widgets/input/auto_complete_field.dart';
 import 'package:perna/widgets/input/outlined_text_form_field.dart';
 
 class CompanyForm extends StatefulWidget {
-  const CompanyForm({this.readOnly = false});
+  CompanyForm({this.bankAccount, this.readOnly = false, this.onSubmmitCompany}) {
+    if (!readOnly) assert(onSubmmitCompany != null);
+  }
 
+  final Function(Company) onSubmmitCompany;
+  final Company bankAccount;
   final bool readOnly;
 
   @override
@@ -18,58 +24,104 @@ class CompanyForm extends StatefulWidget {
 
 class _CompanyFormState extends State<CompanyForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  FocusNode companyNameFocus = FocusNode();
   Company company;
 
-  // TODO: modificar company quando alterar os campos e definir icones
   @override
   Widget build(BuildContext context) {
     return FormContainer(formkey: _formKey, children: <Widget>[
-      // TODO: enum para seleção de estrutura da empresa
-      OutlinedTextFormField(
-          readOnly: true,
+      AutoCompleteField(  
+          readOnly: widget.readOnly,
+          isRequired: true,
+          onFieldSubmitted: (String value) {
+            companyNameFocus.requestFocus();
+            company = company.copyWith(businessType: value);
+          },
           labelText: AppLocalizations.of(context).translate('business_type'),
           icon: Icons.business_center),
       const SizedBox(height: 26),
       OutlinedTextFormField(
-          readOnly: true,
+          readOnly: widget.readOnly,
+          isRequired: true,
+          focusNode: companyNameFocus,
+          onFieldSubmitted: (String value) {
+            company = company.copyWith(companyName: value);
+          },
           labelText: AppLocalizations.of(context).translate('company_name'),
           icon: Icons.short_text),
       const SizedBox(height: 26),
       OutlinedTextFormField(
-          readOnly: true,
+          readOnly: widget.readOnly,
+          isRequired: true,
+          onFieldSubmitted: (String value) {
+            company = company.copyWith(manager: value);
+          },
           labelText: AppLocalizations.of(context).translate('manager'),
           icon: Icons.admin_panel_settings),
       const SizedBox(height: 26),
       OutlinedTextFormField(
-          readOnly: true,
+          readOnly: widget.readOnly,
+          isRequired: true,
+          textInputType: TextInputType.number,
+          onFieldSubmitted: (String value) {
+            company = company.copyWith(companyNumber: value);
+          },
           labelText: AppLocalizations.of(context).translate('company_number'),
           icon: Icons.business),
       const SizedBox(height: 26),
       OutlinedTextFormField(
-          readOnly: true,
+          readOnly: widget.readOnly,
+          isRequired: true,
+          textInputType: TextInputType.number,
+          onFieldSubmitted: (String value) {
+            company = company.copyWith(phone: value);
+          },
           labelText: AppLocalizations.of(context).translate('phone'),
           icon: Icons.contact_phone),
       const SizedBox(height: 26),
       OutlinedTextFormField(
-          readOnly: true,
+          readOnly: widget.readOnly,
+          isRequired: true,
+          onFieldSubmitted: (String value) {
+            company = company.copyWith(address: value);
+          },
           labelText: AppLocalizations.of(context).translate('address'),
           icon: Icons.markunread_mailbox_rounded),
       const SizedBox(height: 26),
-      // TODO: enum para seleção de estrutura da empresa
-      OutlinedTextFormField(
-          readOnly: true,
+      AutoCompleteField(
+          readOnly: widget.readOnly,
+          isRequired: true,
+          onFieldSubmitted: (String value) {
+            company = company.copyWith(structure: value);
+            Navigator.push(
+                context,
+                MaterialPageRoute<BankPage>(
+                    builder: (BuildContext context) => BankPage(
+                      onSubmmitBankAccount: (BankAccount bankAccount) {
+                        widget.onSubmmitCompany(company.copyWith(
+                          bankAccount: bankAccount,
+                        ));
+                      },
+                    )));
+          },
           labelText: AppLocalizations.of(context).translate('structure'),
           icon: Icons.assignment_rounded),
       const SizedBox(height: 26),
       Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
         AddButton(
-          addAndcontinue: true,
+          addAndcontinue: widget.readOnly,
           readOnly: widget.readOnly,
           onPressed: () {
             Navigator.push(
                 context,
                 MaterialPageRoute<BankPage>(
-                    builder: (BuildContext context) => BankPage()));
+                    builder: (BuildContext context) => BankPage(
+                      onSubmmitBankAccount: (BankAccount bankAccount) {
+                        widget.onSubmmitCompany(company.copyWith(
+                          bankAccount: bankAccount,
+                        ));
+                      },
+                    )));
           },
         )
       ])
